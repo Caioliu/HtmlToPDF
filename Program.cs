c1130a282f9b86d08cd00b7cc9aafb4a40f4366a
Caioliu <caioarthur_@hotmail.com>
2024-05-13 17:14:05 -0300

    upload files

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using DinkToPdf;
+using DinkToPdf.Contracts;
+
+class Program {
+    static void Main(string[] args) {
+        var html = File.ReadAllText("Contrato.html");
+
+        var globalSettings = new GlobalSettings {
+            ColorMode = ColorMode.Color,
+            Orientation = Orientation.Portrait,
+            PaperSize = PaperKind.A4,
+        };
+
+        var objectSettings = new ObjectSettings {
+            PagesCount = true,
+            HtmlContent = html,
+            WebSettings = { DefaultEncoding = "utf-8", UserStyleSheet = Path.Combine(Directory.GetCurrentDirectory(), "Contrato.css") },
+        };
+
+        var pdf = new HtmlToPdfDocument() {
+            GlobalSettings = globalSettings,
+            Objects = { objectSettings }
+        };
+
+        var converter = new SynchronizedConverter(new PdfTools());
+        byte[] pdfData = converter.Convert(pdf);
+        File.WriteAllBytes("arquivo41.pdf", pdfData);
+    }
+
+}
